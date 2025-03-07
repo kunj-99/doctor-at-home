858f770e74ab0a3aed09e03bb4cc8c2c08d3c21d
Your Name <your-email@example.com>
2025-03-07 18:23:47 +0530

    book button clic listener

--- app/src/main/java/com/example/thedoctorathomeuser/doctor_details.java
+++ app/src/main/java/com/example/thedoctorathomeuser/doctor_details.java
@@ -1,6 +1,9 @@
 package com.example.thedoctorathomeuser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class doctor_details extends AppCompatActivity {
 
+    Button book ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
+
+        book = findViewById(R.id.bookButton);
+
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(doctor_details.this,book_form.class);
+
+                startActivity(in);
+            }
+        });
 
     }
 }
