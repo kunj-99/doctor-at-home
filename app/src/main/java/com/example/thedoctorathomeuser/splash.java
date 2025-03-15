c48b11339a2863b08718e379fbd42f2b127226ea
Your Name <your-email@example.com>
2025-03-15 17:48:17 +0530

    chengis

---
+++ app/src/main/java/com/example/thedoctorathomeuser/splash.java
@@ -0,0 +1,41 @@
+package com.example.thedoctorathomeuser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Apply fade-in animation
+        ImageView logo = findViewById(R.id.logoImage);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+
+        Handler n = new Handler();
+        n.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent  m = new Intent(splash.this, login.class);
+                startActivity(m);
+
+            }
+        },5000);
+
+
+
+    }
+}
